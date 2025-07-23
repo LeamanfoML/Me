549db84d70a32e486844ae59079133c9cbcad041
LeamanfoML <dezveer2@gmail.com>
2025-07-23 21:27:04 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,18 @@ def generate_token_hash(token: str) -> str:
     return hashlib.sha256(token.encode()).hexdigest()
 
 def validate_nft_data(item: dict) -> bool:
-    """Валидация данных NFT"""
-    required_fields = {'id', 'name', 'model', 'current_bid', 'end_time'}
-    return all(field in item for field in required_fields)
+    """Валидация данных NFT для обоих маркетплейсов"""
+    # Обязательные поля для Portals
+    portals_required = {'id', 'name', 'current_bid', 'end_time'}
+    
+    # Обязательные поля для Tonnel
+    tonnel_required = {'id', 'name', 'current_price', 'end_time'}
+    
+    # Проверяем наличие хотя бы одного набора
+    if all(field in item for field in portals_required):
+        return True
+    
+    if all(field in item for field in tonnel_required):
+        return True
+    
+    return False
