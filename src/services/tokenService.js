83427b17e7ecd8b8f51e8937682cbe13d9131abe
LeamanfoML <dezveer2@gmail.com>
2025-07-24 16:36:11 +0300

    Update tokenService.js

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,1 +1,42 @@
+const cron = require('node-cron');
+const axios = require('axios');
+const { autoTokenInterval, portalsApiUrl, tonnelApiUrl } = require('../config');
+const logger = require('../utils/logger');
 
+class TokenService {
+  constructor() {
+    this.portalsToken = null;
+    this.tonnelToken = null;
+    this.scheduleRefresh();
+  }
+
+  async refreshPortalsToken() {
+    try {
+      const res = await axios.post(`${portalsApiUrl}/auth/update_auth`);
+      this.portalsToken = res.data.token;
+      logger.info('Portals токен обновлён');
+    } catch (err) {
+      logger.error('Не удалось обновить Portals токен: %s', err.message);
+    }
+  }
+
+  async refreshTonnelToken() {
+    try {
+      const res = await axios.post(`${tonnelApiUrl}/auth`);
+      this.tonnelToken = res.data.token;
+      logger.info('Tonnel токен обновлён');
+    } catch (err) {
+      logger.error('Не удалось обновить Tonnel токен: %s', err.message);
+    }
+  }
+
+  scheduleRefresh() {
+    // обновляем токены по расписанию
+    cron.schedule(`*/${autoTokenInterval / 60} * * * *`, () => {
+      this.refreshPortalsToken();
+      this.refreshTonnelToken();
+    });
+  }
+}
+
+module.exports = new TokenService();
