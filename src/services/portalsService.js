9206f2a03d69b3f8988210fa223295867b210df8
LeamanfoML <dezveer2@gmail.com>
2025-07-24 16:34:55 +0300

    Update portalsService.js

--- src/services/portalsService.js
+++ src/services/portalsService.js
@@ -1,1 +1,17 @@
+const axios = require('axios');
+const { portalsApiUrl } = require('../config');
+const logger = require('../utils/logger');
 
+class PortalsService {
+  async fetchAuctions() {
+    try {
+      const res = await axios.get(`${portalsApiUrl}/auctions`);
+      return res.data.auctions; // массив аукционов
+    } catch (err) {
+      logger.error('Ошибка Portals API: %s', err.message);
+      throw err;
+    }
+  }
+}
+
+module.exports = new PortalsService();
