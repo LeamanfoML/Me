1a683e2338c489be6dbadde3b7b5d9aa7e726bbd
LeamanfoML <dezveer2@gmail.com>
2025-07-29 10:16:16 +0300

    Delete src directory

--- src/utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const logger = require('./logger');
-
-function errorHandler(err, req, res, next) {
-  logger.error('Ошибка API: %s', err.message);
-  res.status(500).json({ error: 'Внутренняя ошибка сервера' });
-}
-
-module.exports = errorHandler;
